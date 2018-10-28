8085554965be24ee2f401344621a8ad269b950fa
Raphaël Godart <raphael@digimezzo.com>
2018-10-28 22:08:43 +0100

    Finished the about screen

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -13,11 +13,19 @@ export class InformationComponent implements OnInit {
   public applicationName: string;
   public applicationVersion: string;
   public applicationCopyright: string;
+  public websiteUrl: string;
+  public emailAddress: string;
+  public facebookUrl: string;
+  public twitterUrl: string;
 
   constructor(public dialog: MatDialog) {
     this.applicationName = Constants.applicationName.toUpperCase();
     this.applicationVersion = Constants.applicationVersion;
     this.applicationCopyright = Constants.applicationCopyright;
+    this.websiteUrl = Constants.websiteUrl;
+    this.emailAddress = Constants.emailAddress;
+    this.facebookUrl = Constants.facebookUrl;
+    this.twitterUrl = Constants.twitterUrl;
   }
 
   ngOnInit() {
@@ -32,5 +40,9 @@ export class InformationComponent implements OnInit {
     //   console.log('The dialog was closed');
     //   this.animal = result;
     // });
+  }
+
+  openDonateLink(): void {
+    require('electron').shell.openExternal(Constants.donateUrl);
   }
 }
