9c120421e16eb1040d336e4cbb1d2bc1a6537173
Raphaël Godart <raphael@digimezzo.com>
2018-10-29 13:46:15 +0100

    Fixed the paypal url

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -2,7 +2,7 @@ export class Constants {
     static readonly applicationName: string = "Knowte";
     static readonly applicationVersion: any = require("../../../package.json").version;
     static readonly applicationCopyright: string = "Copyright Digimezzo Ⓒ 2013 - 2018";
-    static readonly donateUrl = "https://www.paypal.com/donate/?token=5lzYAO3uVmz4UM00jQzSD8xgfyqyCMa7KAMygIBGNij2HNBKAcxQ0FwbqTdD2Q7Vg-Yxam&country.x=BE&locale.x=BE";
+    static readonly donateUrl = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MQALEWTEZ7HX8";
     static readonly websiteUrl = "https://www.digimezzo.com";
     static readonly emailAddress = "https://www.digimezzo.com/contact";
     static readonly facebookUrl = "https://www.facebook.com/Digimezzo";
