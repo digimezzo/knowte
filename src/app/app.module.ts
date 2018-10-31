c26071ea7c6f8a681eba45027b6def15c07f9790
Raphaël Godart <raphael@digimezzo.com>
2018-10-31 22:00:19 +0100

    Added CollectionService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ElectronService } from './providers/electron.service';
+import { CollectionService } from './providers/collection.service';
 
 import { WebviewDirective } from './directives/webview.directive';
 
@@ -69,7 +70,7 @@ export function HttpLoaderFactory(http: HttpClient) {
       }
     })
   ],
-  providers: [ElectronService],
+  providers: [ElectronService, CollectionService],
   bootstrap: [AppComponent, BackButtonComponent, MainMenuButtonComponent],
   entryComponents: [
     LicenseDialogComponent
