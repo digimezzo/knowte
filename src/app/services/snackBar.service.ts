b73f73efcfa25571c604fd6d148ed18e20214efa
Raphaël Godart <raphael@digimezzo.com>
2019-01-20 08:37:51 +0100

    Added quill text editor

--- src/app/services/snackBar.service.ts
+++ src/app/services/snackBar.service.ts
@@ -16,47 +16,47 @@ export class SnackBarService {
     }
 
     public collectionAdded(collectionName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.CollectionAdded', {collectionName: collectionName});
+        let message: string = this.translate.instant('SnackBarMessages.CollectionAdded', { collectionName: collectionName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public collectionActivated(collectionName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.CollectionActivated', {collectionName: collectionName});
+        let message: string = this.translate.instant('SnackBarMessages.CollectionActivated', { collectionName: collectionName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public collectionRenamed(collectionName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.CollectionRenamed', {collectionName: collectionName});
+        let message: string = this.translate.instant('SnackBarMessages.CollectionRenamed', { collectionName: collectionName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public collectionDeleted(collectionName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.CollectionDeleted', {collectionName: collectionName});
+        let message: string = this.translate.instant('SnackBarMessages.CollectionDeleted', { collectionName: collectionName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public duplicateCollection(collectionName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.DuplicateCollection', {collectionName: collectionName});
+        let message: string = this.translate.instant('SnackBarMessages.DuplicateCollection', { collectionName: collectionName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public duplicateNotebook(notebookName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.DuplicateNotebook', {notebookName: notebookName});
+        let message: string = this.translate.instant('SnackBarMessages.DuplicateNotebook', { notebookName: notebookName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public notebookAdded(notebookName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.NotebookAdded', {notebookName: notebookName});
+        let message: string = this.translate.instant('SnackBarMessages.NotebookAdded', { notebookName: notebookName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public notebookDeleted(notebookName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.NotebookDeleted', {notebookName: notebookName});
+        let message: string = this.translate.instant('SnackBarMessages.NotebookDeleted', { notebookName: notebookName });
         this.showActionLessSnackBar(message, 2000);
     }
 
     public notebookRenamed(notebookName: string) {
-        let message: string = this.translate.instant('SnackBarMessages.NotebookRenamed', {notebookName: notebookName});
+        let message: string = this.translate.instant('SnackBarMessages.NotebookRenamed', { notebookName: notebookName });
         this.showActionLessSnackBar(message, 2000);
     }
 }
