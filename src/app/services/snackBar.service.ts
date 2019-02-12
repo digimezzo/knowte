7109be2bb14e591c58410dcf61ae006d346fb0eb
Raphaël Godart <raphael@digimezzo.com>
2019-02-12 23:55:43 +0100

    Added OK action to snackbar messages

--- src/app/services/snackBar.service.ts
+++ src/app/services/snackBar.service.ts
@@ -15,48 +15,63 @@ export class SnackBarService {
         });
     }
 
+    private showActionSnackBar(message: string, action: string, durationMillis: number) {
+        this.zone.run(() => {
+            this.snackBar.open(message, action, { duration: durationMillis });
+        });
+    }
+
     public async collectionAddedAsync(collectionName: string) {	
         let message: string = await this.translate.get('SnackBarMessages.CollectionAdded', { collectionName: collectionName }).toPromise();	
-        this.showActionLessSnackBar(message, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async collectionDeletedAsync(collectionName: string) {	
         let message: string = await this.translate.get('SnackBarMessages.CollectionDeleted', { collectionName: collectionName }).toPromise();	
-        this.showActionLessSnackBar(message, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async collectionRenamedAsync(collectionName: string) {	
         let message: string = await this.translate.get('SnackBarMessages.CollectionRenamed', { collectionName: collectionName }).toPromise();	
-        this.showActionLessSnackBar(message, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async collectionActivatedAsync(collectionName: string) {
         let message: string = await this.translate.get('SnackBarMessages.CollectionActivated', { collectionName: collectionName }).toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async duplicateCollectionAsync(collectionName: string) {
         let message: string = await this.translate.get('SnackBarMessages.DuplicateCollection', { collectionName: collectionName }).toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async duplicateNotebookAsync(notebookName: string) {
         let message: string = await this.translate.get('SnackBarMessages.DuplicateNotebook', { notebookName: notebookName }).toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async noteDeleteBlockedAsync(noteTitle: string) {
         let message: string = await this.translate.get('SnackBarMessages.NoteDeleteBlocked', { noteTitle: noteTitle }).toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async noteAlreadyOpenAsync() {
         let message: string = await this.translate.get('SnackBarMessages.NoteAlreadyOpen').toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 
     public async noteTitleCannotBeEmptyAsync() {
         let message: string = await this.translate.get('SnackBarMessages.NoteTitleCannotBeEmpty').toPromise();
-        this.showActionLessSnackBar(message, 2000);
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
+        this.showActionSnackBar(message, action, 2000);	
     }
 }
