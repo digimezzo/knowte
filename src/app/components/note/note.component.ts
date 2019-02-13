336ecf32d2ebf8bef97124a931597ff9be11bf7d
Raphaël Godart <raphael.godart@gmail.com>
2019-02-13 22:51:46 +0100

    Cleaned up the note toolbar

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -67,7 +67,30 @@ export class NoteComponent implements OnInit {
 
         let notePlaceHolder: string = await this.translateService.get('Notes.NotePlaceholder').toPromise();
 
+        let toolbarOptions: any = [
+            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            ['blockquote', 'code-block'],
+
+            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+            // [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+            // [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+            // [{ 'direction': 'rtl' }],                         // text direction
+
+            // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+            // [{ 'font': [] }],
+            // [{ 'align': [] }],
+
+            ['clean']                                         // remove formatting button
+        ];
+
         this.quill = new Quill('#editor', {
+            modules: {
+                toolbar: toolbarOptions
+            },
             placeholder: notePlaceHolder,
             theme: 'snow',
         });
