0e4f8a97463daec1b229724787e1886ca41b7095
Raphaël <raphael@digimezzo.com>
2019-03-11 19:49:10 +0100

    Building the import dialog

--- src/app/components/dialogs/importFromOldVersionDialog/importFromOldVersionDialog.component.ts
+++ src/app/components/dialogs/importFromOldVersionDialog/importFromOldVersionDialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { remote } from 'electron';
+import log from 'electron-log';
 
 @Component({
     selector: 'importfromoldversion-dialog',
@@ -9,6 +11,22 @@ export class ImportFromOldVersionDialogComponent implements OnInit {
     constructor() {
     }
 
+    public isDirectoryChosen: boolean = false;
+    public selectedDirectory: string = "";
+
     ngOnInit() {
+    }
+
+    public selectDirectory(): void {
+        let selectedDirectories: string[] = remote.dialog.showOpenDialog({ properties: ['openDirectory'] });
+
+        if (selectedDirectories && selectedDirectories.length > 0) {
+            this.selectedDirectory = selectedDirectories[0];
+            this.isDirectoryChosen = true;
+        }
+    }
+
+    public startImport(): void {
+
     }
 }
