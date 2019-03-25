8db8056dcdf2ba7ce1f7e58a49c3573d9b865078
Raphaël <raphael@digimezzo.com>
2019-03-25 06:12:41 +0100

    Added a language collection

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,3 +1,5 @@
+import { Language } from "./language";
+
 export class Constants {
     static readonly applicationName: string = require("../../../package.json").name;
     static readonly applicationVersion: string = require("../../../package.json").version;
@@ -13,7 +15,7 @@ export class Constants {
     static readonly defaultCollection: string = "Default";
     static readonly allNotesNotebookId: string = "0";
     static readonly unfiledNotesNotebookId: string = "1";
-  
+
     static readonly allCategory: string = "all";
     static readonly todayCategory: string = "today";
     static readonly yesterdayCategory: string = "yesterday";
@@ -33,20 +35,25 @@ export class Constants {
     static readonly closeNoteEvent: string = "0b7877ac-ca71-4cf5-a5a5-96798be7c5be";
     static readonly deleteNoteEvent: string = "0896531e-ee16-4407-b4e5-120b55c6e82f";
 
-    static readonly externalComponents: any[] = [
-        { name: "Angular", description: "Angular is a development platform for building mobile and desktop web applications using Typescript/JavaScript and other languages.", url: "https://angular.io/", licenseUrl: "https://github.com/angular/angular/blob/master/LICENSE" },
-        { name: "Angular Material", description: "Component infrastructure and Material Design components for Angular.", url: "https://material.angular.io/", licenseUrl: "https://github.com/angular/material2/blob/master/LICENSE" },
-        { name: "Electron", description: "The Electron framework lets you write cross-platform desktop applications using JavaScript, HTML and CSS. It is based on Node.js and Chromium.", url: "https://electronjs.org/", licenseUrl: "https://github.com/electron/electron/blob/master/LICENSE" },
-        { name: "electron-log", description: "Just a simple logging module for your Electron or NW.js application. No dependencies. No complicated configuration. Just require and use. It can be used without Electron.", url: "https://github.com/megahertz/electron-log", licenseUrl: "https://github.com/megahertz/electron-log/blob/master/LICENSE" },
-        { name: "electron-store", description: "Simple data persistence for your Electron app or module. Save and load user preferences, app state, cache, etc.", url: "https://github.com/sindresorhus/electron-store", licenseUrl: "https://github.com/sindresorhus/electron-store/blob/master/license" },
-        { name: "electron-window-state", description: "A library to store and restore window sizes and positions for your Electron app.", url: "https://github.com/mawie81/electron-window-state", licenseUrl: "https://github.com/mawie81/electron-window-state/blob/master/license" },
-        { name: "Font Awesome Free", description: "The iconic SVG, font, and CSS toolkit.", url: "https://github.com/FortAwesome/Font-Awesome", licenseUrl: "https://github.com/FortAwesome/Font-Awesome/blob/master/LICENSE.txt" },
-        { name: "fs-extra", description: "Adds file system methods that aren't included in the native fs module and adds promise support to the fs methods.", url: "https://github.com/jprichardson/node-fs-extra", licenseUrl: "https://github.com/jprichardson/node-fs-extra/blob/master/LICENSE" },
-        { name: "LokiJS", description: "A fast, in-memory document-oriented datastore for node.js, browser and cordova.", url: "http://lokijs.org/", licenseUrl: "https://github.com/techfort/LokiJS/blob/master/LICENSE.txt" },
-        { name: "Material Design icons", description: "Material Design icons by Google.", url: "https://github.com/google/material-design-icons", licenseUrl: "https://github.com/google/material-design-icons/blob/master/LICENSE" },
-        { name: "Moment", description: "Parse, validate, manipulate, and display dates in javascript.", url: "http://momentjs.com", licenseUrl: "https://github.com/moment/moment/blob/develop/LICENSE" },
-        { name: "Nano ID", description: "A tiny (141 bytes), secure, URL-friendly, unique string ID generator for JavaScript.", url: "https://github.com/ai/nanoid", licenseUrl: "https://github.com/ai/nanoid/blob/master/LICENSE" },
-        { name: "Quill", description: "Quill is a modern WYSIWYG editor built for compatibility and extensibility.", url: "https://quilljs.com/", licenseUrl: "https://github.com/quilljs/quill/blob/develop/LICENSE" },
-        { name: "sanitize-filename", description: "Sanitize a string to be safe for use as a filename by removing directory paths and invalid characters.", url: "https://github.com/parshap/node-sanitize-filename", licenseUrl: "https://github.com/parshap/node-sanitize-filename/blob/master/LICENSE.md" }
+    static readonly languages: Language[] = [
+        { code: "en", name: "English" },
+        { code: "fr", name: "Français" }
     ];
+
+    static readonly externalComponents: any[] = [
+    { name: "Angular", description: "Angular is a development platform for building mobile and desktop web applications using Typescript/JavaScript and other languages.", url: "https://angular.io/", licenseUrl: "https://github.com/angular/angular/blob/master/LICENSE" },
+    { name: "Angular Material", description: "Component infrastructure and Material Design components for Angular.", url: "https://material.angular.io/", licenseUrl: "https://github.com/angular/material2/blob/master/LICENSE" },
+    { name: "Electron", description: "The Electron framework lets you write cross-platform desktop applications using JavaScript, HTML and CSS. It is based on Node.js and Chromium.", url: "https://electronjs.org/", licenseUrl: "https://github.com/electron/electron/blob/master/LICENSE" },
+    { name: "electron-log", description: "Just a simple logging module for your Electron or NW.js application. No dependencies. No complicated configuration. Just require and use. It can be used without Electron.", url: "https://github.com/megahertz/electron-log", licenseUrl: "https://github.com/megahertz/electron-log/blob/master/LICENSE" },
+    { name: "electron-store", description: "Simple data persistence for your Electron app or module. Save and load user preferences, app state, cache, etc.", url: "https://github.com/sindresorhus/electron-store", licenseUrl: "https://github.com/sindresorhus/electron-store/blob/master/license" },
+    { name: "electron-window-state", description: "A library to store and restore window sizes and positions for your Electron app.", url: "https://github.com/mawie81/electron-window-state", licenseUrl: "https://github.com/mawie81/electron-window-state/blob/master/license" },
+    { name: "Font Awesome Free", description: "The iconic SVG, font, and CSS toolkit.", url: "https://github.com/FortAwesome/Font-Awesome", licenseUrl: "https://github.com/FortAwesome/Font-Awesome/blob/master/LICENSE.txt" },
+    { name: "fs-extra", description: "Adds file system methods that aren't included in the native fs module and adds promise support to the fs methods.", url: "https://github.com/jprichardson/node-fs-extra", licenseUrl: "https://github.com/jprichardson/node-fs-extra/blob/master/LICENSE" },
+    { name: "LokiJS", description: "A fast, in-memory document-oriented datastore for node.js, browser and cordova.", url: "http://lokijs.org/", licenseUrl: "https://github.com/techfort/LokiJS/blob/master/LICENSE.txt" },
+    { name: "Material Design icons", description: "Material Design icons by Google.", url: "https://github.com/google/material-design-icons", licenseUrl: "https://github.com/google/material-design-icons/blob/master/LICENSE" },
+    { name: "Moment", description: "Parse, validate, manipulate, and display dates in javascript.", url: "http://momentjs.com", licenseUrl: "https://github.com/moment/moment/blob/develop/LICENSE" },
+    { name: "Nano ID", description: "A tiny (141 bytes), secure, URL-friendly, unique string ID generator for JavaScript.", url: "https://github.com/ai/nanoid", licenseUrl: "https://github.com/ai/nanoid/blob/master/LICENSE" },
+    { name: "Quill", description: "Quill is a modern WYSIWYG editor built for compatibility and extensibility.", url: "https://quilljs.com/", licenseUrl: "https://github.com/quilljs/quill/blob/develop/LICENSE" },
+    { name: "sanitize-filename", description: "Sanitize a string to be safe for use as a filename by removing directory paths and invalid characters.", url: "https://github.com/parshap/node-sanitize-filename", licenseUrl: "https://github.com/parshap/node-sanitize-filename/blob/master/LICENSE.md" }
+];
 }
