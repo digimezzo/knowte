634eccfe9a0839deb0b55c3c4685501ecc0259a4
Raphaël <raphael@digimezzo.com>
2019-03-26 20:52:44 +0100

    snackbar notification display time is based on letter count

--- src/app/services/snackBar.service.ts
+++ src/app/services/snackBar.service.ts
@@ -9,51 +9,58 @@ export class SnackBarService {
     constructor(private zone: NgZone, private translate: TranslateService, private snackBar: MatSnackBar) {
     }
 
-    private showActionLessSnackBar(message: string, durationMillis: number) {
+    private showActionLessSnackBar(message: string) {
         this.zone.run(() => {
-            this.snackBar.open(message, '', { duration: durationMillis });
+            this.snackBar.open(message, '', { duration: this.calculateDuration(message) });
         });
     }
 
-    private showActionSnackBar(message: string, action: string, durationMillis: number) {
+    private showActionSnackBar(message: string, action: string) {
         this.zone.run(() => {
-            this.snackBar.open(message, action, { duration: durationMillis });
+            this.snackBar.open(message, action, { duration: this.calculateDuration(message) });
         });
     }
-    
+
+    private calculateDuration(message: string): number {
+        // See: https://ux.stackexchange.com/questions/11203/how-long-should-a-temporary-notification-toast-appear
+        // Estimate that 1 character is read every 75 milliseconds
+        // Reading speed of 150 words per minute, with an average of 5.8 characters per word.
+        return Math.min(Math.max(message.length * 75, 2000), 7000);
+    }
+
     public async duplicateCollectionAsync(collection: string) {
         let message: string = await this.translate.get('SnackBarMessages.DuplicateCollection', { collection: collection }).toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 
     public async duplicateNotebookAsync(notebookName: string) {
         let message: string = await this.translate.get('SnackBarMessages.DuplicateNotebook', { notebookName: notebookName }).toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 
     public async noteTitleCannotBeEmptyAsync() {
         let message: string = await this.translate.get('SnackBarMessages.NoteTitleCannotBeEmpty').toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 
     public async closeNoteBeforeChangingCollections() {
         let message: string = await this.translate.get('SnackBarMessages.CloseNoteBeforeChangingCollections').toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 3000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 
     public async noteExportedAsync(noteTitle: string) {
         let message: string = await this.translate.get('SnackBarMessages.NoteExported', { noteTitle: noteTitle }).toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 
     public async invalidNoteFileAsync() {
         let message: string = await this.translate.get('SnackBarMessages.InvalidNoteFile').toPromise();
-        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();	
-        this.showActionSnackBar(message, action, 2000);	
+        let action: string = await this.translate.get('SnackBarActions.Ok').toPromise();
+        this.showActionSnackBar(message, action);
     }
 }
