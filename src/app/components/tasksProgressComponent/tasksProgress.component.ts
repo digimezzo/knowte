c0d189a4481236d87706b0e7f6cf44f61e05fa8e
Raphaël Godart <raphael@digimezzo.com>
2019-06-20 13:37:49 +0200

    Finished adding tasks progress

--- src/app/components/tasksProgressComponent/tasksProgress.component.ts
+++ src/app/components/tasksProgressComponent/tasksProgress.component.ts
@@ -6,12 +6,40 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: ['./tasksProgress.component.scss']
 })
 export class TasksProgressComponent implements OnInit {
+    private _closedTasksCount : number;
+    private _totalTasksCount: number;
+
     constructor() {
     }
+    
+    public get closedTasksCount() : number {
+        return this._closedTasksCount;
+    }
 
-    @Input() public closedTasksCount: number;
-    @Input() public totalTasksCount: number;
+    @Input()
+    public set closedTasksCount(v : number) {
+        this._closedTasksCount = v;
+        this.calculateWidth();
+    }
+
+    public get totalTasksCount() : number {
+        return this._totalTasksCount;
+    }
+
+    @Input()
+    public set totalTasksCount(v : number) {
+        this._totalTasksCount = v;
+        this.calculateWidth();
+    }
+
+    public width: Number;
 
     public ngOnInit(): void {
+        this.calculateWidth();
+    }
+
+    private calculateWidth(): void {
+        let maxWidth: number = 100;
+        this.width = (this.closedTasksCount / this.totalTasksCount) * maxWidth;
     }
 }
