d8310a097c146b2ee3293444041754714c8434f6
Raphaël <raphael@digimezzo.com>
2019-09-13 20:58:04 +0200

    Started refactoring theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ElectronService } from './services/electron.service';
 import { CollectionService } from './services/collection/collection.service';
 import { Router } from '@angular/router';
@@ -14,47 +14,20 @@ import { TranslatorService } from './services/translator/translator.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  private globalEmitter = remote.getGlobal('globalEmitter');
-  private themeChangedListener: any = this.applyTheme.bind(this);
-
-  constructor(public electron: ElectronService, public router: Router, private appearance: AppearanceService,
+export class AppComponent implements OnInit {
+  constructor(public electron: ElectronService, public router: Router, public appearance: AppearanceService,
     private translator: TranslatorService, private collection: CollectionService,
     private settings: Settings, private overlayContainer: OverlayContainer) {
 
-    this.applyTheme(this.settings.colorTheme);
+    this.appearance.applySettingsTheme();
     this.translator.applyLanguage();
   }
 
-  public selectedTheme: string;
-
-  public ngOnDestroy(): void {
-    this.globalEmitter.on(Constants.themeChangedEvent, this.themeChangedListener);
-  }
-
   public ngOnInit(): void {
-    this.globalEmitter.on(Constants.themeChangedEvent, this.themeChangedListener);
-    
     let showWelcome: boolean = !this.collection.hasStorageDirectory;
 
     if (showWelcome) {
       this.router.navigate(['/welcome']);
     }
-  }
-
-  private applyTheme(themeName: string): void {
-    // Apply theme to app container
-    this.selectedTheme = themeName;
-
-    // Apply theme to components in the overlay container
-    // https://gist.github.com/tomastrajan/ee29cd8e180b14ce9bc120e2f7435db7
-    let overlayContainerClasses: DOMTokenList = this.overlayContainer.getContainerElement().classList;
-    let themeClassesToRemove: string[] = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
-
-    if (themeClassesToRemove.length) {
-      overlayContainerClasses.remove(...themeClassesToRemove);
-    }
-
-    overlayContainerClasses.add(themeName);
   }
 }
--- src/app/services/appearance/appearance.service.ts
+++ src/app/services/appearance/appearance.service.ts
@@ -11,10 +11,14 @@ import { OverlayContainer } from '@angular/cdk/overlay';
 })
 export class AppearanceService {
     private globalEmitter = remote.getGlobal('globalEmitter');
+    private themeChangedListener: any = this.applyTheme.bind(this);
 
-    constructor(private settings: Settings, private logger: Logger, private overlayContainer: OverlayContainer) { }
-
+    constructor(private settings: Settings, private logger: Logger, private overlayContainer: OverlayContainer) { 
+        this.globalEmitter.on(Constants.themeChangedEvent, this.themeChangedListener);
+    }
+   
     public colorThemes: ColorTheme[] = Constants.colorThemes;
+    public selectedTheme: string;
 
     public setTheme(themeName: string): void {
         this.settings.colorTheme = themeName;
@@ -22,4 +26,24 @@ export class AppearanceService {
         // Global event because all windows need to be notified
         this.globalEmitter.emit(Constants.themeChangedEvent, themeName);
     }
+
+    public applySettingsTheme(){
+        this.applyTheme(this.settings.colorTheme);
+    }
+
+    private applyTheme(themeName: string): void {
+        // Apply theme to app container
+        this.selectedTheme = themeName;
+    
+        // Apply theme to components in the overlay container
+        // https://gist.github.com/tomastrajan/ee29cd8e180b14ce9bc120e2f7435db7
+        let overlayContainerClasses: DOMTokenList = this.overlayContainer.getContainerElement().classList;
+        let themeClassesToRemove: string[] = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+    
+        if (themeClassesToRemove.length) {
+          overlayContainerClasses.remove(...themeClassesToRemove);
+        }
+    
+        overlayContainerClasses.add(themeName);
+      }
 }
