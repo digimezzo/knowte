4aa7ced4582089a55ab1875cedda43e8dabf7acd
Raphaël <raphael@digimezzo.com>
2019-11-02 10:33:31 +0100

    Renamed project from angular-electron to knowte-electron

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AngularElectronPage } from './app.po';
 import { browser, element, by } from 'protractor';
 
-describe('angular-electron App', () => {
+describe('knowte-electron App', () => {
   let page: AngularElectronPage;
 
   beforeEach(() => {
