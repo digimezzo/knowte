5376b8727f3a1da05a09d60625dfe9f384a1f791
Digimezzo <raphael@digimezzo.com>
2020-03-24 18:57:04 +0100

    Added FontSize and unit tests

---
+++ src/app/core/font-size.ts
@@ -0,0 +1,7 @@
+export class FontSize {
+    constructor(public normalSize: number) {
+    }
+
+    public largeSize: number = this.normalSize * 1.42;
+    public largerSize: number = this.normalSize * 1.72;
+}
