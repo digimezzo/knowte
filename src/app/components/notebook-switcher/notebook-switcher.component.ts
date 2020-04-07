bb8d810cbd7e4269e32cbec8b555c884e6238011
Digimezzo <raphael@digimezzo.com>
2020-04-07 08:20:47 +0200

    Added folder and files for notebook switcher

---
+++ src/app/components/notebook-switcher/notebook-switcher.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-notebook-switcher',
+    templateUrl: './notebook-switcher.component.html',
+    styleUrls: ['./notebook-switcher.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class NotebookSwitcherComponent implements OnInit, OnDestroy {
+    constructor() {
+    }
+    public ngOnDestroy(): void {
+
+    }
+
+    public ngOnInit(): void {
+
+    }
+}
