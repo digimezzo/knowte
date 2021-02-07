433a9efc339c0b638585af98543a2ad02f30532d
Digimezzo Raphaël <raphael@digimezzo.com>
2021-02-07 11:23:08 +0100

    The collection panes are working as they should

---
+++ src/app/components/collection/collection-notebooks/collection-notebooks.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-collection-notebooks',
+    templateUrl: './collection-notebooks.component.html',
+    styleUrls: ['./collection-notebooks.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class CollectionNotebooksComponent implements OnInit {
+    constructor() {}
+
+    public ngOnInit(): void {}
+}
---
+++ src/app/components/collection/collection-notes/collection-notes.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-collection-notes',
+    templateUrl: './collection-notes.component.html',
+    styleUrls: ['./collection-notes.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class CollectionNotesComponent implements OnInit {
+    constructor() {}
+
+    public ngOnInit(): void {}
+}
