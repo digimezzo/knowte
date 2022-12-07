665126f071801ea88ab1ceef7698a890c55725aa
Digimezzo <digimezzo@outlook.com>
2022-12-07 07:45:36 +0100

    Updated links

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,4 +1,4 @@
-import { ColorScheme as ColorScheme } from './color-scheme';
+import { ColorScheme } from './color-scheme';
 import { FontSize } from './font-size';
 import { Language } from './language';
 
@@ -7,8 +7,8 @@ export class Constants {
     public static readonly applicationName: string = require('../../../package.json').name;
     public static readonly applicationVersion: string = require('../../../package.json').version;
     public static readonly applicationCopyright: string = 'Copyright Digimezzo Ⓒ 2013 - 2020';
-    public static readonly donateUrl: string = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MQALEWTEZ7HX8';
-    public static readonly websiteUrl: string = 'https://www.digimezzo.com';
+    public static readonly donateUrl: string = 'https://digimezzo.github.io/site/donate';
+    public static readonly websiteUrl: string = 'https://digimezzo.github.io/site/';
     public static readonly twitterUrl: string = 'https://twitter.com/digimezzo';
     public static readonly githubUrl: string = 'https://github.com/digimezzo';
     public static readonly collectionsDirectory: string = 'Knowte collections';
