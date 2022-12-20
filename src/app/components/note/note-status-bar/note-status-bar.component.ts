9eaad5b5f1548aafd7ef7a56448c3c8bd5eb92a2
Digimezzo <digimezzo@outlook.com>
2022-12-20 22:30:30 +0100

    Fixed a bug

--- src/app/components/note/note-status-bar/note-status-bar.component.ts
+++ src/app/components/note/note-status-bar/note-status-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BaseSettings } from '../../../core/base-settings';
+import { SpellCheckService } from '../../../services/spell-check/spell-check.service';
 import { SpellCheckBottomSheetComponent } from '../bottom-sheets/spell-check-bottom-sheet/spell-check-bottom-sheet.component';
 import { TextSizeBottomSheetComponent } from '../bottom-sheets/text-size-bottom-sheet/text-size-bottom-sheet.component';
 
@@ -10,7 +11,7 @@ import { TextSizeBottomSheetComponent } from '../bottom-sheets/text-size-bottom-
     templateUrl: 'note-status-bar.component.html',
 })
 export class NoteStatusBarComponent {
-    constructor(public settings: BaseSettings, private bottomSheet: MatBottomSheet) {}
+    constructor(public spellCheckService: SpellCheckService, public settings: BaseSettings, private bottomSheet: MatBottomSheet) {}
 
     public openTextSizeBottomSheet(): void {
         this.bottomSheet.open(TextSizeBottomSheetComponent);
