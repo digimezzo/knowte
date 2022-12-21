4b8f0a9c905c41dde9051378876751fa73948c00
Digimezzo <digimezzo@outlook.com>
2022-12-21 08:44:53 +0100

    Fixed build issue

--- src/app/components/note/note-status-bar/note-status-bar.component.ts
+++ src/app/components/note/note-status-bar/note-status-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BaseSettings } from '../../../core/base-settings';
 import { SpellCheckService } from '../../../services/spell-check/spell-check.service';
@@ -12,6 +12,9 @@ import { TextSizeBottomSheetComponent } from '../bottom-sheets/text-size-bottom-
 })
 export class NoteStatusBarComponent {
     constructor(public spellCheckService: SpellCheckService, public settings: BaseSettings, private bottomSheet: MatBottomSheet) {}
+
+    @Input()
+    public isMarked: boolean;
 
     public openTextSizeBottomSheet(): void {
         this.bottomSheet.open(TextSizeBottomSheetComponent);
