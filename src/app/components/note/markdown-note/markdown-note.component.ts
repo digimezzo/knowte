d54b4b2bbb53a865318d18c2d7d49b1f3d02d265
Digimezzo Raphaël <digimezzo@outlook.com>
2025-07-29 22:30:42 +0200

    Seems to work. Needs testing.

--- src/app/components/note/markdown-note/markdown-note.component.ts
+++ src/app/components/note/markdown-note/markdown-note.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild, ViewEncapsulation} from '@angular/core';
 import { INoteEditor } from '../note-editor/i-note-editor';
 
 @Component({
@@ -7,17 +7,44 @@ import { INoteEditor } from '../note-editor/i-note-editor';
     styleUrls: ['./markdown-note.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
-export class MarkdownNoteComponent {
+export class MarkdownNoteComponent implements AfterViewInit {
+    @ViewChild('highlightedContent') highlightedContent: ElementRef<HTMLPreElement>;
+    @ViewChild('markdownInput') markdownInput: ElementRef<HTMLTextAreaElement>;
+    
     @Input()
     public noteEditor: INoteEditor;
+
+    public ngAfterViewInit() {
+        const input = this.markdownInput.nativeElement;
+        const pre = this.highlightedContent.nativeElement;
+
+        input.addEventListener('scroll', () => {
+            pre.scrollTop = input.scrollTop;
+            pre.scrollLeft = input.scrollLeft;
+        });
+    }
+
+    public onContentChange() {
+        const input = this.markdownInput.nativeElement;
+        const pre = this.highlightedContent.nativeElement;
+
+        // Ensure pre scrolls to match textarea after content change
+        pre.scrollTop = input.scrollTop;
+        pre.scrollLeft = input.scrollLeft;
+    }
 
     public toggleIsEditing(): void {
         this.noteEditor.isEditing = !this.noteEditor.isEditing;
     }
 
-    highlightMarkdown(text: string): string {
+    public highlightMarkdown(text: string): string {
         if(!text) {
             return '';
+        }
+
+        // Handle final newlines (See: https://css-tricks.com/creating-an-editable-textarea-that-supports-syntax-highlighted-code/)
+        if(text[text.length-1] == "\n") { // If the last character is a newline character
+            text += " "; // Add a placeholder space character to the final line 
         }
         
         const escaped = text
