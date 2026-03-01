3b15f4df0432e38ad3fb2c8d889aff137f6a0230
Digimezzo <digimezzo@outlook.com>
2026-03-01 22:17:09 +0100

    Add Snap configuration and update package metadata

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -3,6 +3,30 @@ const { getFullVersion } = require('./get-package-information.js');
 const config = {
     appId: 'com.digimezzo.knowte',
     productName: 'Knowte',
+    snap: {
+        base: 'core22', // Must match build server (currently Ubuntu 22.04)
+        grade: 'stable',
+        confinement: 'strict',
+        plugs: [
+            // REQUIRED for Electron desktop apps
+            'desktop',
+            'desktop-legacy',
+            'wayland',
+            'x11',
+            'unity7',
+            'opengl',
+            'audio-playback',
+            'browser-support',
+            'network',
+            'network-bind',
+            'gsettings',
+            'screen-inhibit-control',
+
+            // File access
+            'home',
+            'removable-media',
+        ],
+    },
     nsis: {
         shortcutName: 'Knowte',
         perMachine: true,
@@ -22,11 +46,11 @@ const config = {
     },
     linux: {
         target: ['AppImage', 'deb', 'rpm', 'pacman', 'snap'],
+        category: 'Utility',
         artifactName: `\${productName}-${getFullVersion()}.\${ext}`,
-        desktop: {
-            Name: 'Knowte',
-            Terminal: 'false',
-        },
+        synopsis: 'Cross platform note taking application.',
+        description:
+            'Knowte is a note taking application that allows you to quickly and easily organize and find your notes.',
     },
 };
 
